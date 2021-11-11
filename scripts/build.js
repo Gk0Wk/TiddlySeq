17816276dd7336bc6206b2d55f3dcbf6234b3312
Ke Wang <sttot0134@gmail.com>
2021-11-12 03:08:43 +0800

    Rewrite it

---
+++ scripts/build.js
@@ -0,0 +1,135 @@
+const path = require('path');
+const {
+    execSync
+} = require('child_process');
+
+/** 项目路径 */
+const repoFolder = path.join(path.dirname(__filename), '..');
+// cross-env TIDDLYWIKI_PLUGIN_PATH='node_modules/tiddlywiki/plugins/published' TIDDLYWIKI_THEME_PATH='${wikiFolderName}/themes'
+/** 插件路径 */
+process.env.TIDDLYWIKI_PLUGIN_PATH = `${repoFolder}/plugins`;
+/** 主题路径 */
+process.env.TIDDLYWIKI_THEME_PATH = `${repoFolder}/themes`;
+/**
+ * 执行命令行指令，并打印该指令
+ * @param {string} command 要执行的命令
+ * @param {object} options execSync的参数
+ */
+function shell(command, options) {
+    if (options !== undefined) options = {};
+    console.log(String(execSync(command, {
+        cwd: repoFolder,
+        ...options,
+    })));
+}
+/**
+ * 执行命令行指令，并打印该指令，同时忽略任何错误
+ * @param {string} command 要执行的命令
+ * @param {object} options execSync的参数
+ */
+function shellI(command, options) {
+    try {
+        shell(command, options);
+    } catch (error) {}
+}
+
+/**
+ * 构建在线HTML版本：核心JS和资源文件不包括在HTML中， 下载后不能使用
+ * @param {string} distDir 目标路径，空或者不填则默认为'dist'
+ * @param {string} htmlName HTML名称，空或者不填则默认为'index.html'
+ * @param {boolean} minify 是否最小化JS和HTML，默认为true
+ */
+function buildOnlineHTML(distDir, htmlName, minify) {
+    if (typeof distDir !== 'string' || distDir.length === 0) distDir = 'dist';
+    if (typeof htmlName !== 'string' || htmlName.length === 0) htmlName = 'index.html';
+    if (typeof minify !== 'boolean') minify = true;
+
+    // 清空生成目标
+    shell(`rm -rf ${distDir}`);
+
+    // 静态资源拷贝
+    shellI(`cp -r public/ ${distDir} &> /dev/null`);
+    shellI(`cp tiddlers/favicon.ico ${distDir}/favicon.ico &> /dev/null`);
+    shellI(`cp tiddlers/TiddlyWikiIconWhite.png ${distDir}/TiddlyWikiIconWhite.png &> /dev/null`);
+    shellI(`cp tiddlers/TiddlyWikiIconBlack.png ${distDir}/TiddlyWikiIconBlack.png &> /dev/null`);
+
+    // 构建HTML
+    shell('cp -r tiddlers/ tmp_tiddlers_backup &> /dev/null'); // 备份 因为下面有改变tiddler的field的操作(媒体文件全部转为canonical)
+    shell(`tiddlywiki . --output ${distDir}` +
+        ' --setfield \'[is[image]] [is[binary]] [type[application/msword]] [type[image/svg+xml]]\' _canonical_uri $:/core/templates/canonical-uri-external-image text/plain' +
+        ' --setfield \'[is[image]] [is[binary]] [type[application/msword]] [type[image/svg+xml]]\' text "" text/plain' + /* 注意这一步也会把所有媒体文件的内容变成空的 */
+        ' --rendertiddler $:/core/save/offline-external-js index-raw.html text/plain "" publishFilter "-[is[draft]]"' +
+        ' --rendertiddler $:/core/templates/tiddlywiki5.js tiddlywikicore.js text/plain'
+    );
+    shell('cp -r tmp_tiddlers_backup/* tiddlers &> /dev/null'); // 恢复被清空内容的媒体文件
+    // 删除非二进制文件，后缀类型定义在 boot.js ($tw.utils.registerFileType(...))
+    shellI('cd tiddlers && rm *.meta *.tid *.multids *.tiddler *.recipe *.txt *.css *.html *.htm *.hta *.js *.json *.md *.bib &> /dev/null');
+    shellI(`mkdir ${distDir}/images &> /dev/null`);
+    shell(`mv tiddlers/*.* ${distDir}/images &> /dev/null`); // 非二进制文件也就是资源文件的拷贝
+    shell('rm -rf tiddlers && mv tmp_tiddlers_backup tiddlers &> /dev/null'); // 还原
+
+    // 最小化：核心JS和HTML
+    if (minify) {
+        shellI(`uglifyjs ${distDir}/tiddlywikicore.js -c -m --v8 --webkit --ie --output '${distDir}/tiddlywikicore-'${'$(echo $(tiddlywiki . --version) | awk \'{print $1}\')'}'.js' && rm ${distDir}/tiddlywikicore.js`);
+        shellI(`html-minifier-terser -c scripts/html-minifier-terser.config.json -o ${distDir}/${htmlName} ${distDir}/index-raw.html && rm ${distDir}/index-raw.html`);
+    } else {
+        shellI(`mv ${distDir}/tiddlywikicore.js '${distDir}/tiddlywikicore-'${'$(echo $(tiddlywiki . --version) | awk \'{print $1}\')'}'.js'`);
+        shellI(`mv ${distDir}/index-raw.html ${distDir}/${htmlName}`);
+    }
+}
+
+/**
+ * 构建离线HTML版本：核心JS和资源文件包括在HTML中， 下载后可以使用(就是单文件版本的wiki)
+ * @param {string} distDir 目标路径，空或者不填则默认为'dist'
+ * @param {string} htmlName HTML名称，空或者不填则默认为'index.html'
+ * @param {boolean} minify 是否最小化JS和HTML，默认为true
+ */
+function buildOfflineHTML(distDir, htmlName, minify) {
+    if (typeof distDir !== 'string' || distDir.length === 0) distDir = 'dist';
+    if (typeof htmlName !== 'string' || htmlName.length === 0) htmlName = 'index.html';
+    if (typeof minify !== 'boolean') minify = true;
+
+    // 构建HTML
+    shell(`tiddlywiki . --output ${distDir}` +
+        ' --rendertiddler $:/core/save/all index-raw.html text/plain "" publishFilter "-[is[draft]]"'
+    );
+
+    // 最小化：HTML
+    if (minify) {
+        shellI(`html-minifier-terser -c scripts/html-minifier-terser.config.json -o ${distDir}/${htmlName} ${distDir}/index-raw.html && rm ${distDir}/index-raw.html`);
+    } else {
+        shellI(`mv ${distDir}/index-raw.html ${distDir}/${htmlName}`);
+    }
+}
+
+/**
+ * 构建插件源
+ * @param {string} pluginFilter 要发布插件的过滤器，默认为空
+ * @param {string} distDir 目标路径，空或者不填则默认为'dist/library'
+ * @param {boolean} minify 是否最小化HTML，默认为true
+ */
+function buildLibrary(pluginFilter, distDir, minify) {
+    if (typeof pluginFilter !== 'string' || htmlName.length === 0) pluginFilter = '';
+    if (typeof distDir !== 'string' || distDir.length === 0) distDir = 'dist/library';
+    if (typeof minify !== 'boolean') minify = true;
+
+    shell(`tiddlywiki . --output ${distDir}` +
+        ' --makelibrary $:/UpgradeLibrary' +
+        ` --savelibrarytiddlers $:/UpgradeLibrary ${pluginFilter} recipes/library/tiddlers/ $:/UpgradeLibrary/List` +
+        ' --savetiddler $:/UpgradeLibrary/List recipes/library/tiddlers.json' +
+        ' --rendertiddler $:/plugins/tiddlywiki/pluginlibrary/library.template.html index-raw.html text/plain'
+    );
+
+    // 最小化：HTML
+    if (minify) {
+        shellI(`html-minifier-terser -c scripts/html-minifier-terser.config.json -o ${distDir}/index.html ${distDir}/index-raw.html && rm ${distDir}/index-raw.html`);
+    } else {
+        shellI(`mv ${distDir}/index-raw.html ${distDir}/${htmlName}`);
+    }
+}
+
+module.exports = {
+    buildOnlineHTML: buildOnlineHTML,
+    buildOfflineHTML: buildOfflineHTML,
+    buildLibrary: buildLibrary,
+};
