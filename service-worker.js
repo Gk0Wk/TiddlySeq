a0dafaca4f87b090be9fb04bd261b112cb98452d
Gk0Wk <Gk0Wk@users.noreply.github.com>
2021-12-02 10:52:39 +0000

    deploy: f79c7c84df096acba60e9c29401873e1774c1642

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"0eb9346991354d7b9454473741baff74","url":"index.html"},{"revision":"7f4a3ee3b98420bb07a15472ac06d5fe","url":"offline.html"},{"revision":"ab7f41fcafda368590e1e93c58b208a8","url":"tiddlywikicore-5.2.0.js"}]);
+precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"4386b7156c3ed0b3ac28d0dd5060f719","url":"index.html"},{"revision":"15d9800b6e30324641529dacaa7365f1","url":"offline.html"},{"revision":"ab7f41fcafda368590e1e93c58b208a8","url":"tiddlywikicore-5.2.0.js"}]);
 
 registerRoute(
   /\.css$/,
