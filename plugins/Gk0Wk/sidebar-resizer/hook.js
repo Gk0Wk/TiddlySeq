4b85d7d9f1bcd546eda5ecd55959b7111548d001
Ke Wang <sttot0134@gmail.com>
2021-12-04 14:53:59 +0800

    Update SidebarResizer to v0.0.3

--- plugins/Gk0Wk/sidebar-resizer/hook.js
+++ plugins/Gk0Wk/sidebar-resizer/hook.js
@@ -3,33 +3,56 @@
     if (typeof window !== 'undefined' && window.document) {
         var sidebarResizerNode = null;
         var canResize = false;
+        var body = document.querySelector('body');
+        if (!body) return;
+
+        function dragBegin(event) {
+            canResize = true;
+            if (window.PointerEvent) body.setPointerCapture(event.pointerId);
+            // When drag begins, prevent text selecting (for event.preventDefault cannot work in Mozilla)
+            body.style.useSelect = 'none';
+            body.style.MozUserSelect = 'none';
+        }
+
+        function dragEnd(event) {
+            if (canResize) {
+                canResize = false;
+                if (window.PointerEvent) body.releasePointerCapture(event.pointerId);
+                body.style.useSelect = 'auto';
+                body.style.MozUserSelect = 'auto';
+            }
+        }
+
         $tw.hooks.addHook('th-page-refreshed', function() {
             if (sidebarResizerNode && sidebarResizerNode.ownerDocument.contains(sidebarResizerNode)) return;
-            var body = document.querySelector('body');
-            if (!body) return;
             sidebarResizerNode = document.querySelector('#gk0wk-sidebar-resize-area');
             if (!sidebarResizerNode) return;
             canResize = false;
 
             // Hide Sidebar
-            function hideSideBar() {
+            function hideSideBar(event) {
                 $tw.wiki.setText('$:/state/sidebar', null, null, 'no');
-                canResize = false;
+                dragEnd(event);
             }
 
             // Drag to resize
             function drag(event) {
                 if (!canResize) return;
+                if (!event) event = window.event;
+                // Prevent event pass
+                event.preventDefault(event);
+                event.stopPropagation(event);
+                event.stopImmediatePropagation(event);
+                event.returnValue = false;
                 var widthPercent = 100 - (event.clientX / window.innerWidth) * 100;
-                if (widthPercent > 80) return;
+                if (widthPercent > 80) return false;
                 if ((window.innerWidth - event.clientX) < 100) {
                     $tw.wiki.setText('$:/themes/tiddlywiki/vanilla/metrics/sidebarwidth', null, null, (30000 / window.innerWidth) + 'vw');
-                    hideSideBar();
-                    return;
+                    hideSideBar(event);
+                } else {
+                    $tw.wiki.setText('$:/themes/tiddlywiki/vanilla/metrics/sidebarwidth', null, null, widthPercent + 'vw');
                 }
-                $tw.wiki.setText('$:/themes/tiddlywiki/vanilla/metrics/sidebarwidth', null, null, widthPercent + 'vw');
-                event.preventDefault();
-                event.stopPropagation();
+                return false;
             }
 
             // Double click to hide sidebar
@@ -37,28 +60,12 @@
             // Detect whter PointerEvent is supported
             if (window.PointerEvent) {
                 // PointerEvent = Mouse + Touch
-                function dragBegin(event) {
-                    canResize = true;
-                    body.setPointerCapture(event.pointerId);
-                }
-
-                function dragEnd(event) {
-                    canResize = false;
-                    body.releasePointerCapture(event.pointerId);
-                }
                 sidebarResizerNode.addEventListener('pointerdown', dragBegin);
                 body.addEventListener('pointerup', dragEnd);
                 body.addEventListener('blur', dragEnd);
                 body.addEventListener('pointermove', drag, true);
             } else {
                 // If not
-                function dragBegin() {
-                    canResize = true;
-                }
-
-                function dragEnd() {
-                    canResize = false;
-                }
                 sidebarResizerNode.addEventListener('mousedown', dragBegin);
                 body.addEventListener('mouseup', dragEnd);
                 body.addEventListener('blur', dragEnd);
