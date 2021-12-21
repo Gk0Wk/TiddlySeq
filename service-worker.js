9205c646f51aea35bdbf06d50e2963cbd01023c8
Gk0Wk <Gk0Wk@users.noreply.github.com>
2021-12-21 06:05:18 +0000

    deploy: e3a8e2885bdfaf307b44196d996df63f169d845b

--- service-worker.js
+++ service-worker.js
@@ -11,7 +11,7 @@ const { CacheFirst, StaleWhileRevalidate } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { precacheAndRoute, matchPrecache } = workbox.precaching;
 
-precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"08d6a36f63b6e1ce27428918002c864d","url":"images/SidebarResizerDemo.gif"},{"revision":"2e3a48566155a05677d6dfd868e1a6e0","url":"index.html"},{"revision":"d9b0b084ba47905682c34f15f68da6ef","url":"offline.html"},{"revision":"88bc0a8bc68dfdc8f7d716b2e8e3962d","url":"tiddlywikicore-5.2.1.js"}]);
+precacheAndRoute([{"revision":"2202d1024158299c249bb2eeb50dc494","url":"favicon.ico"},{"revision":"08d6a36f63b6e1ce27428918002c864d","url":"images/SidebarResizerDemo.gif"},{"revision":"3fe1cda57d157a46d5272aaa196f345b","url":"index.html"},{"revision":"d495533f66ccb3cb09edbbe80bbe6e38","url":"offline.html"},{"revision":"88bc0a8bc68dfdc8f7d716b2e8e3962d","url":"tiddlywikicore-5.2.1.js"}]);
 
 registerRoute(
   /\.css$/,
