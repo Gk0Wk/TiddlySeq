d3066de706af12b5c739c843a8fd15f11574a8bb
Ke Wang <sttot0134@gmail.com>
2022-01-22 09:05:07 +0800

    Hook the jQuery

---
+++ plugins/Gk0Wk/jQuery/hook-jquery.js
@@ -0,0 +1,27 @@
+/*\
+title: $:/core/modules/startup/hook-jquery.js
+type: application/javascript
+module-type: startup
+
+Hook jQuery object to wiki.
+
+\*/
+(function(){
+
+/*jslint node: true, browser: true */
+/*global $tw: false */
+"use strict";
+
+// Export name and synchronous status
+exports.name = "hook-jquery";
+exports.platforms = ["browser"];
+exports.after = ["render"];
+exports.synchronous = true;
+exports.startup = function() {
+    try {
+        window.jQuery = window.$ = require('$:/plugins/Gk0Wk/jQuery/jquery.min.js');
+    } catch (e) {
+        console.error(e);
+    }
+};
+})();
